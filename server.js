fe6815ede6e1898251d487b30abf80e0a69442b3
Pavan Kalyan <pavankalyan.yes@gmail.com>
2024-04-22 12:05:07 +0530

    Set up basic Express server with Socket.io

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
+// set static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = 3000 || process.env.PORT;
 
