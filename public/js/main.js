22dc7dc2e2c6b4cb2a2dd3a640281ad6c35ad143
Pavan Kalyan <pavankalyan.yes@gmail.com>
2024-04-22 13:58:55 +0530

    testing: fetch the text from input field

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,16 @@
+const chatForm = document.getElementById('chat-form');
+
 const socket = io();
 
 socket.on('message', message =>{
     console.log(message);
+})
+
+chatForm.addEventListener('submit', (e) =>{
+    e.preventDefault();
+
+    const messageInput = document.getElementById('msg');
+    const message = messageInput.value;
+
+    console.log('message from you:', message);
 })
