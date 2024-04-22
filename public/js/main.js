dd2eb2f5945fee656f7fd98a8b3582b4497d6cf3
Pavan Kalyan <pavankalyan.yes@gmail.com>
2024-04-22 21:52:47 +0530

    scroll down

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,16 @@
 const chatForm = document.getElementById('chat-form');
+const chatMessages = document.querySelector('.chat-messages');
 
 const socket = io();
 
+
+//message from server
 socket.on('message', message =>{
     console.log(message);
+    outputMessage(message);
+
+    //scroll down
+    chatMessages.scrollTop = chatMessages.scrollHeight;
 })
 
 chatForm.addEventListener('submit', (e) =>{
@@ -17,4 +24,15 @@ chatForm.addEventListener('submit', (e) =>{
     console.log('the messagr from the input field:', message);
     //emit mssage to the server
     socket.emit('chatMessage', message)
-})
+});
+
+//Output message to DOM
+function outputMessage(message){
+    const div = document.createElement('div');
+    div.classList.add('message');
+    div.innerHTML = `<p class="meta">Brad <span>9:12pm</span></p>
+    <p class="text">
+        ${message}
+    </p>`;
+    document.querySelector('.chat-messages').appendChild(div)
+}
