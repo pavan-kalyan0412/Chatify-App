3fa922e2633fcc7bbda30ec87575194989fe9800
Pavan Kalyan <pavankalyan.yes@gmail.com>
2024-04-22 12:05:49 +0530

    Set up basic Express server with Socket.io

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const http = require('http');
+const socketio = require('socket.io');
+ const server = http.createServer(app);
+ const io = socketio(server);
+
 
 // set static folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', (socket) =>{
+    console.log('New WS connected');
+});
 
 const PORT = 3000 || process.env.PORT;
 
